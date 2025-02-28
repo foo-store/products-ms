05d9f2bddf06e65447f1bca83e8fdb7e876de05f
bjgonzalesgUNT <t022700120@unitru.edu.pe>
2025-02-28 13:09:42 -0500

    Agregar verificación de productos y mejorar la gestión de errores en el servicio de productos

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,20 +1,25 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { ProductsService } from './products.service';
 import { PaginationDto } from 'src/common/dto';
 import { CreateProductDto } from './dto';
+import { ProductsService } from './products.service';
 
 @Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @MessagePattern('product.create')
-  createProduct(@Payload() productDto: CreateProductDto) {
-    return this.productsService.create(productDto);
+  async createProduct(@Payload() productDto: CreateProductDto) {
+    return await this.productsService.create(productDto);
   }
 
   @MessagePattern('product.list')
-  getProducts(@Payload() paginationDto: PaginationDto) {
-    return this.productsService.getAll(paginationDto);
+  async getProducts(@Payload() paginationDto: PaginationDto) {
+    return await this.productsService.getAll(paginationDto);
+  }
+
+  @MessagePattern('product.verify')
+  async verifyProduct(@Payload() productsId: number[]) {
+    return await this.productsService.verify(productsId);
   }
 }
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,6 +1,12 @@
-import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Product } from '@prisma/client';
 import { NATS_SERVICE } from 'src/common/constants';
 import { PaginationDto } from 'src/common/dto';
 import { PaginationService } from 'src/common/modules/pagination/pagination.service';
@@ -10,8 +16,8 @@ import { CreateProductDto } from './dto';
 export class ProductsService extends PrismaClient implements OnModuleInit {
   private logger = new Logger('ProductsService');
 
-  onModuleInit() {
-    this.$connect();
+  async onModuleInit() {
+    await this.$connect();
     this.logger.log('Connected to the database');
   }
 
@@ -34,10 +40,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       });
 
       return newProduct;
-    } catch (error) {
+    } catch (error: unknown) {
       throw new RpcException({
         statusCode: 500,
-        message: error.message,
+        message: error,
       });
     }
   }
@@ -52,5 +58,20 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const total = await this.product.count();
 
     return this.paginationService.create(paginationDto, data, total);
+  }
+
+  async verify(productsId: number[]): Promise<Product[]> {
+    const products = await this.product.findMany({
+      where: { id: { in: productsId } },
+    });
+
+    if (products.length !== productsId.length) {
+      throw new RpcException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'Some products were not found',
+      });
+    }
+
+    return products;
   }
 }
